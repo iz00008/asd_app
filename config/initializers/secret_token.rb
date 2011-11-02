f1bdc2367dd1ce43702e4b3a8435288b4b153f33
iz00008 <iz00008@surrey.ac.uk>
2011-11-02 00:09:45 +0000

    dokimi1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsdApp::Application.config.secret_token = '6d069999774270ea5bf7ffdca71b9d93710b65d77cd04cf35d924c0ee7e4bbad49202e1c8048b006cd16d10da51e8de13ecf36001fb2d9d2f2f43f10cf10062b'
